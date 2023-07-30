25186c003d8af1f6623622220feb957e62b4e753
spiism <sp.iism@gmail.com>
2023-07-31 08:46:58 +1200

    add button size and add to cart

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,22 +8,32 @@ export default function Home() {
   return (
     <div className="container mx-auto p-8">
       <div className="flex flex-wrap">
-        {/* First box with image */}
         <div className="w-full md:w-1/2 p-4">
           <Image src={item} alt="item-cart" />
         </div>
-        {/* Second box with text */}
         <div className="w-full md:w-1/2 p-4">
           <div className="mb-4">
-            <h2 className="text-xl font-bold">Title</h2>
+            <h2 className="text-xl font-bold">Classic Tee</h2>
+            <p className="text-gray-600">$75.00</p>
             <p className="text-gray-600">
               Description Lorem ipsum dolor sit amet, consectetur adipiscing
               elit.
             </p>
           </div>
-          <div>
+          <div className="space-x-2">
             <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
-              Add
+              S
+            </button>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+              M
+            </button>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+              L
+            </button>
+          </div>
+          <div className="mt-5">
+            <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
+              Add to my cart
             </button>
           </div>
         </div>
